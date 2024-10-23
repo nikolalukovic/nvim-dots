fe67253b2b28ce3425494bdf7ddbaac87e73f056
Nikola Lukovic <nikola.lukovic@live.com>
2024-10-23 19:13:20 +0200

    Added gleam

--- lua/plugins/astrolsp.lua
+++ lua/plugins/astrolsp.lua
@@ -24,6 +24,7 @@ return {
           "ts",
           "cs",
           "js",
+          "gleam",
         },
         ignore_filetypes = { -- disable format on save for specified filetypes
           -- "python",
@@ -40,7 +41,7 @@ return {
     },
     -- enable servers that you already have installed without mason
     servers = {
-      -- "pyright"
+      "gleam"
     },
     -- customize language server configuration options passed to `lspconfig`
     ---@diagnostic disable: missing-fields
