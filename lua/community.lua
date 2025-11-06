1e2ff809e94c3499cb98a2b72c498f88ad9e31de
Nikola Lukovic <nikola.lukovic@live.com>
2025-11-06 09:51:52 +0100

    Init V5

---
+++ lua/community.lua
@@ -0,0 +1,18 @@
+-- AstroCommunity: import any community modules here
+-- We import this file in `lazy_setup.lua` before the `plugins/` folder.
+-- This guarantees that the specs are processed before any user plugins.
+
+---@type LazySpec
+return {
+  "AstroNvim/astrocommunity",
+  {
+    import = "astrocommunity.colorscheme.gruvbox-nvim",
+    opts = {
+      contrast = "hard",
+      italic = {
+        operators = true,
+      },
+    },
+  },
+  { import = "astrocommunity.pack.lua" },
+}
